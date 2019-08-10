6cab432c4f7251bc7144147cd6a7b0ad5f1783f7
ovitaliy <ovitaliy.rudenko@gmail.com>
2019-08-10 15:35:39 +0300

    Replace turnFlash with changeFlashMode

--- lib/ui/camera/camera.dart
+++ lib/ui/camera/camera.dart
@@ -283,7 +283,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
 
     try {
       await controller.initialize();
-      await controller.turnFlash(true);
+      await controller.changeFlashMode(FlashMode.single);
     } on CameraException catch (e) {
       _showCameraException(e);
     }
@@ -326,7 +326,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
       return null;
     }
 
-    final Directory extDir = await getExternalStorageDirectory();
+    final Directory extDir = await getApplicationDocumentsDirectory();
     final String dirPath = '${extDir.path}/Movies/flutter_test';
     await Directory(dirPath).create(recursive: true);
     final String filePath = '$dirPath/${timestamp()}.mp4';
